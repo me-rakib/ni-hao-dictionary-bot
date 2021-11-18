77b06b5ebc43f7835ab42019dd3bfeba7ba082bb
Rakib Hasan <rakib.cs132@gmail.com>
2021-11-18 14:22:43 +0600

    translation to english added

--- main.py
+++ main.py
@@ -2,17 +2,26 @@ import discord
 from discord.ext import commands
 import json
 from difflib import get_close_matches
+from googletrans import Translator
 
+# loading data
 data = json.load(open("data.json"))
 
+# period as prefix to call bot command
+bot = commands.Bot(command_prefix='.')
 
+# creating an instance of translator
+translator = Translator()
+
+
+# find word meaning
 def get_meaning(w):
     keys = data.keys()
     close_match = get_close_matches(w, keys)
     if w in keys:
         return data[w]
     elif w.title() in keys:
-        return data[w.title()]
+        return data[w.tit.le()]
     elif w.upper() in keys:
         return data[w.upper()]
     elif len(close_match) >= 1:
@@ -21,18 +30,32 @@ def get_meaning(w):
         return "My bad! This might be a new word!"
 
 
-bot = commands.Bot(command_prefix='.')
+# translation
+def get_translation(text, dest='en'):
+    return translator.translate(text, dest).text
 
 
+# bot commands
 @bot.command()
-async def meaning(ctx, word):
+async def helpme(ctx):
+    await ctx.reply('Hi! Nǐn hǎo here. I\'ll be happy to help you.\n.meaning yourword - to find the meaning.\n.translate yoursentence - for English translation')
+
+
+# get meaning
+@bot.command()
+async def meaning(ctx, *, word):
     temp = get_meaning(word.lower())
     output = ''
     if type(temp) == list:
         output = temp[0]
     else:
         output = temp
-
     await ctx.reply(output)
+
+
+# get translation
+@bot.command()
+async def translate(ctx, *, word):
+    await ctx.reply(get_translation(word))
 
 bot.run('OTEwNjA0NjU1NzE1ODIzNjQ2.YZVQrA.wSbnmHpovBpvcM_nwbKsFb7oDPI')
