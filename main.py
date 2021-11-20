312a18ff209ce2038950f50e06887592433e6f87
Rakib Hasan <rakib.cs132@gmail.com>
2021-11-20 23:43:27 +0600

    synonym antonym added

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ import discord
 from discord.ext import commands, tasks
 import json
 from difflib import get_close_matches
-# from googletrans import Translator
 from deep_translator import GoogleTranslator
+from nltk.corpus import wordnet
 from dotenv import load_dotenv
 from os import getenv
 import random
@@ -15,9 +15,6 @@ keys = data.keys()
 
 # period as prefix to call bot command
 bot = commands.Bot(command_prefix='.')
-
-# creating an instance of translator
-# translator = Translator()
 
 
 # find word meaning
@@ -51,11 +48,36 @@ def get_random_meaning(meaning_list):
 
 
 # translation
-# def get_translation(str, dest='en'):
-#     return translator.translate(str, dest).text
-
 def get_translation(str, dest='en'):
     return GoogleTranslator(source='auto', target=dest).translate(str)
+
+
+# fetch synonyms
+def get_synonym(word):
+    word_list = []
+    for syn in wordnet.synsets(word):
+        for lm in syn.lemmas():
+            word_list.append(lm.name())
+    return word_list
+
+
+# fetch antonyms
+def get_antonym(word):
+    word_list = []
+    for syn in wordnet.synsets(word):
+        for lm in syn.lemmas():
+            if lm.antonyms():
+                word_list.append(lm.antonyms()[0].name())
+    return word_list
+
+
+# print synonyms or antonyms
+def print_list(list_contain, word, word_list):
+    if len(word_list) == 0:
+        return 'My bad! I haven\'t found anything'
+    else:
+        return f'{list_contain} words of \'{word}\': {str(word_list).replace("]", "").replace("[", "")} etc.'
+
 
 
 # replacing ' and " from string
@@ -75,27 +97,39 @@ async def helpme(ctx):
 
 # get meaning
 @bot.command()
-async def meaning(ctx, *, str):
-    temp = get_meaning(replace_quote(str).lower())
+async def meaning(ctx, *, txt):
+    temp = get_meaning(replace_quote(txt).lower())
     await ctx.reply(get_random_meaning(temp))
 
 
 # get english translations
 @bot.command()
-async def translate(ctx, *, str):
-    await ctx.reply(get_translation(replace_quote(str)))
+async def translate(ctx, *, txt):
+    await ctx.reply(get_translation(replace_quote(txt)))
 
 
 # get hindi translation
 @bot.command()
-async def TTH(ctx, *, str):
-    await ctx.reply(get_translation(replace_quote(str), 'hi'))
+async def TTH(ctx, *, txt):
+    await ctx.reply(get_translation(replace_quote(txt), 'hi'))
 
 
 # get bangla translation
 @bot.command()
-async def TTB(ctx, *, str):
-    await ctx.reply(get_translation(replace_quote(str), 'bn'))
+async def TTB(ctx, *, txt):
+    await ctx.reply(get_translation(replace_quote(txt), 'bn'))
+
+
+# get synonyms
+@bot.command()
+async def synonym(ctx, txt):
+    await ctx.reply(print_list('Similar', txt, get_synonym(txt)[:5]))
+
+
+# get antonym
+@bot.command()
+async def antonym(ctx, txt):
+    await ctx.reply(print_list('Opposite', txt, get_antonym(txt)[:5]))
 
 
 @bot.event
@@ -103,7 +137,7 @@ async def on_ready():
     daily_word.start()
 
 
-@tasks.loop(hours=24)
+@tasks.loop(hours=1)
 async def daily_word():
     word = get_random_key()
     temp = get_meaning(word)
